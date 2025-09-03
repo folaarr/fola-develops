e8d066f9e2325d72df4b2e5d591d2625f540d8bd
Folajimi Abolade <folajimiabolade@gmail.com>
2025-09-03 11:09:23 +0100

    Accept render ai html respoonse defeat

--- supplements/ai_skeleton.py
+++ supplements/ai_skeleton.py
@@ -17,54 +17,27 @@ headers = {
     "X-goog-api-key": gemini_key
 }
 
-system_instructions = "Answer should not be more than 325 words, if any of your response requires any at HTML content at any part of the response, wrap each tag in the HTML content around with <xmp> opening and </xmp> closing tags."
-
-# in the HTML content around  # <xmp> opening and </xmp> closing tags
-
-#  as your response will be rendered on an html page and we don't want your response messing with the HTML content
-
-# replace < with &lt; and > with &gt; in the html content, let there be a <br> between every parent and child in the HTML content, that means, a <br> both between their opening and closing tags and wrap all elements in your response around with the <code> opening and </code> closing tags
-
-# , if you are also explaining the function of an HTML tag in your HTML content, instead of using &lt;b&gt; turns the text bold, use <pre>&lt;b&gt;</pre> turns the text bold
+system_instructions = "Answer should not be more than 325 words."
 
 
 def to_html(markdown_text):
     html = markdown2.markdown(markdown_text)
     soup = BeautifulSoup(html, "html.parser")
-    # for tag in soup.find_all(True):
-    #     tag["class"] = "text assistant-message"
     for tag in soup.find_all("a"):
         tag["class"] = "text"
     return soup.prettify()
 
 
 def chat_ai(mes_sage):
-    data = {
-        "contents": [
-            {
-                "role": "user",
-                "parts": [
-                    {
-                        "text": system_instructions
-                    }
-                ]
-            },
-            {
-                "role": "user",
-                "parts": [
-                    {
-                        "text": mes_sage
-                    }
-                ]
-            },
-        ]
-    }
+    data = {"contents": [
+        {"role": "user", "parts": [{"text": system_instructions}]},
+        {"role": "user", "parts": [{"text": mes_sage}]},
+    ]}
     response = requests.post(url, headers=headers, data=json.dumps(data))
     json_response = response.json()
-    print(json_response)
     if "error" in json_response:
-        markdown_text = "Service unavailable, please try again in 15 minutes."
-        html_output = "<p>Service unavailable, please try again in 15 minutes.</p>"
+        markdown_text = f"Error. {json_response["error"]["message"]}"
+        html_output = f"<p>Error. {json_response["error"]["message"]}</p>"
     else:
         markdown_text = json_response["candidates"][0]["content"]["parts"][0]["text"]
         html_output = to_html(markdown_text)
@@ -89,9 +62,8 @@ def identify_chat(i_d):
     data = {"contents": accumulated_chat}
     response = requests.post(url, headers=headers, data=json.dumps(data))
     json_response = response.json()
-    print(json_response)
     if "error" in json_response:
-        return "Service unavailable."
+        return f"{json_response["error"]["message"][:25]}..."
     else:
         return json_response["candidates"][0]["content"]["parts"][0]["text"]
     
@@ -103,12 +75,9 @@ def message_ai(contents, new_message):
     response = requests.post(url, headers=headers, data=json.dumps(data))
     json_response = response.json()
     if "error" in json_response:
-        markdown_text = "Service unavailable, please try again in 15 minutes."
-        html_output = "<p>Service unavailable, please try again in 15 minutes.</p>"
+        markdown_text = f"Error. {json_response["error"]["message"]}"
+        html_output = f"<p>Error. {json_response["error"]["message"]}</p>"
     else:
         markdown_text = json_response["candidates"][0]["content"]["parts"][0]["text"]
         html_output = to_html(markdown_text)
     return {"raw_output": markdown_text, "html_output": html_output}
-
-
-print("error" in {'error': {'code': 503, 'message': 'The model is overloaded. Please try again later.', 'status': 'UNAVAILABLE'}})
